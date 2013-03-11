455e7f923fdbf543450678c5c4109a17f4f10aeb
Jonathan Klein <jonathan.n.klein@gmail.com>
2013-03-11 08:42:23 -0400

    Adding Google Analytics tracking code

--- includes/header.php
+++ includes/header.php
@@ -30,6 +30,16 @@ date_default_timezone_set('America/New_York');
               js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
               fjs.parentNode.insertBefore(js, fjs);
             }(document, 'script', 'facebook-jssdk'));
+
+            var _gaq = _gaq || [];
+            _gaq.push(['_setAccount', 'UA-39188570-1']);
+            _gaq.push(['_trackPageview']);
+
+            (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+            })();
         </script>
 
         <!-- Start header -->
