fc795337dcdd43e150ad3984acd6e231dfcc6017
Jonathan Klein <jonathan.n.klein@gmail.com>
2013-07-03 10:12:09 -0400

    Updating summer schedule

--- schedule.php
+++ schedule.php
@@ -1,5 +1,5 @@
 <?php
-$page_name = 'SCHEDULE';
+$page_name = 'SUMMER SCHEDULE - July 1st to August 31st';
 
 require 'includes/header.php';
 ?>
@@ -15,6 +15,17 @@ require 'includes/header.php';
                     <th>Thursday</th>
                     <th>Friday</th>
                     <th>Saturday</th>
+                </tr>
+                <tr>
+                    <th>&nbsp;</th>
+                    <td colspan="5">
+                        <h1>Summer Taekwondo</h1>
+                        9:00am-12:15pm (half)<br/>
+                        9:00am-3:00pm (full)<br/>
+                        1 Session is 2 Weeks<br/>
+                        <a href="summer.php">Sign up now!</a>
+                    </td>
+                    <th>&nbsp;</th>
                 </tr>
                 <tr>
                     <td>11:00-12:00</td>
@@ -35,45 +46,36 @@ require 'includes/header.php';
                     <td>&nbsp;</td>
                 </tr>
                 <tr>
-                    <td>15:50-16:30</td>
+                    <td>16:30-17:10</td>
                     <td class="under_seven">Little Tigers<br />(under 7 years old)</td>
                     <td></td>
                     <td class="under_seven">Little Tigers<br />(under 7 years old)</td>
                     <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td>16:30-17:15</td>
-                    <td class="kids_a">Children (A)</td>
-                    <td class="under_seven">Little Tigers<br />(under 7 years old)</td>
-                    <td class="kids_a">Children (A)</td>
-                    <td class="under_seven">Little Tigers<br />(under 7 years old)</td>
                     <td></td>
                     <td></td>
                 </tr>
                 <tr>
                     <td>17:15-18:00</td>
+                    <td class="kids_a">Youth</td>
                     <td></td>
-                    <td class="kids_b">Children (B)</td>
+                    <td class="kids_a">Youth</td>
                     <td></td>
-                    <td class="kids_b">Children (B)</td>
-                    <td class="kids_sparring">Children Sparring</td>
+                    <td></td>
                     <td></td>
                 </tr>
                 <tr>
                     <td>18:00-19:00</td>
                     <td class="adult">Adults &amp; Teens</td>
-                    <td class="kids_c">Children (C)</td>
+                    <td class="adult">Adults &amp; Teens</td>
                     <td></td>
-                    <td class="kids_c">Children (C)</td>
-                    <td class="adult">Adult Sparring</td>
+                    <td></td>
+                    <td></td>
                     <td></td>
                 </tr>
                 <tr>
                     <td>19:00-20:00</td>
                     <td></td>
-                    <td class="adult">Adults &amp; Teens</td>
+                    <td></td>
                     <td></td>
                     <td class="adult">Adults &amp; Teens</td>
                     <td></td>
