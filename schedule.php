59c9aefb2728866ef8be01ae26f5a98a813f128f
Jonathan Klein <jonathan.n.klein@gmail.com>
2013-07-10 08:01:24 -0400

    Adding fall/spring schedule

--- schedule.php
+++ schedule.php
@@ -43,7 +43,12 @@ require 'includes/header.php';
                     <td>&nbsp;</td>
                     <td>&nbsp;</td>
                     <td>&nbsp;</td>
-                    <td>&nbsp;</td>
+                    <td class="birthday" rowspan="6">
+                        Birthday Party<br/>Available<br/><br/>
+                        <img src="assets/cake.png" height="100px" width="100px" /><br/><br/>
+                        Ask<br/>
+                        For Details<br/>
+                    </td>
                 </tr>
                 <tr>
                     <td>16:30-17:10</td>
@@ -51,7 +56,6 @@ require 'includes/header.php';
                     <td></td>
                     <td></td>
                     <td class="under_seven">Little Tigers<br />(under 7 years old)</td>
-                    <td></td>
                     <td></td>
                 </tr>
                 <tr>
@@ -61,13 +65,11 @@ require 'includes/header.php';
                     <td></td>
                     <td class="kids_a">Youth</td>
                     <td></td>
-                    <td></td>
                 </tr>
                 <tr>
                     <td>18:00-19:00</td>
                     <td class="adult">Adults &amp; Teens</td>
                     <td class="adult">Adults &amp; Teens</td>
-                    <td></td>
                     <td></td>
                     <td></td>
                     <td></td>
@@ -79,6 +81,90 @@ require 'includes/header.php';
                     <td></td>
                     <td class="adult">Adults &amp; Teens</td>
                     <td></td>
+                 </tr>
+            </table>
+        </div>
+
+        <!-- Fall Schedule -->
+        <br/><br/>
+        <div class="row">
+            <div class="span12" style="text-align:center;color:black">
+                <h4>— FALL/SPRING SCHEDULE - September 1st 2013 to June 30th 2014 —</h4>
+            </div>
+        </div>
+
+        <div class="row">
+            <table class="grid_schedule">
+                <tr>
+                    <th>&nbsp;</th>
+                    <th>Monday</th>
+                    <th>Tuesday</th>
+                    <th>Wednesday</th>
+                    <th>Thursday</th>
+                    <th>Friday</th>
+                    <th>Saturday</th>
+                </tr>
+                <tr>
+                    <td>11:00-12:00</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td class="adult">Adults &amp; Teens</td>
+                </tr>
+                <tr class="blank">
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td class="birthday" rowspan="6">
+                        Birthday Party<br/>Available<br/><br/>
+                        <img src="assets/cake.png" height="100px" width="100px" /><br/><br/>
+                        Ask<br/>
+                        For Details<br/>
+                    </td>
+                </tr>
+                <tr>
+                    <td>3:50-4:30</td>
+                    <td class="under_seven">Little Tigers<br />(under 7 years old)</td>
+                    <td></td>
+                    <td class="under_seven">Little Tigers<br />(under 7 years old)</td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>4:30-5:15</td>
+                    <td class="kids_a">Youth</td>
+                    <td class="under_seven">Little Tigers<br />(under 7 years old)</td>
+                    <td class="kids_a">Youth</td>
+                    <td class="under_seven">Little Tigers<br />(under 7 years old)</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>5:15-6:00</td>
+                    <td></td>
+                    <td class="kids_a">Youth</td>
+                    <td></td>
+                    <td class="kids_a">Youth</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>6:00-7:00</td>
+                    <td class="adult">Adults &amp; Teens</td>
+                    <td class="kids_a">Youth</td>
+                    <td></td>
+                    <td class="kids_a">Youth</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>7:00-8:00</td>
+                    <td></td>
+                    <td class="adult">Adults &amp; Teens</td>
+                    <td></td>
+                    <td class="adult">Adults &amp; Teens</td>
                     <td></td>
                 </tr>
             </table>
