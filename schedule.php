f43363d2778875882b2d40fd64b53b1f747e6c2c
Jonathan Klein <jonathan.n.klein@gmail.com>
2013-09-03 17:58:52 -0400

    Removing summer schedule

--- schedule.php
+++ schedule.php
@@ -1,98 +1,10 @@
 <?php
-$page_name = 'SUMMER SCHEDULE - July 1st to August 31st';
+$page_name = 'FALL/SPRING SCHEDULE - September 3rd 2013 to June 30th 2014';
 
 require 'includes/header.php';
 ?>
 <section class="schedule">
     <div class="container">
-        <div class="row">
-            <table class="grid_schedule">
-                <tr>
-                    <th>&nbsp;</th>
-                    <th>Monday</th>
-                    <th>Tuesday</th>
-                    <th>Wednesday</th>
-                    <th>Thursday</th>
-                    <th>Friday</th>
-                    <th>Saturday</th>
-                </tr>
-                <tr>
-                    <th>&nbsp;</th>
-                    <td colspan="5">
-                        <h1>Summer Taekwondo</h1>
-                        9:00am-12:15pm (half)<br/>
-                        9:00am-3:00pm (full)<br/>
-                        1 Session is 2 Weeks<br/>
-                        <a href="summer.php">Sign up now!</a>
-                    </td>
-                    <th>&nbsp;</th>
-                </tr>
-                <tr>
-                    <td>11:00-12:00</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td class="adult">Adults &amp; Teens</td>
-                </tr>
-                <tr class="blank">
-                    <td>&nbsp;</td>
-                    <td>&nbsp;</td>
-                    <td>&nbsp;</td>
-                    <td>&nbsp;</td>
-                    <td>&nbsp;</td>
-                    <td>&nbsp;</td>
-                    <td class="birthday" rowspan="6">
-                        Birthday Party<br/>Available<br/><br/>
-                        <img src="assets/cake.png" height="100px" width="100px" /><br/><br/>
-                        Ask<br/>
-                        For Details<br/>
-                    </td>
-                </tr>
-                <tr>
-                    <td>16:30-17:10</td>
-                    <td class="under_seven">Little Tigers<br />(under 7 years old)</td>
-                    <td></td>
-                    <td></td>
-                    <td class="under_seven">Little Tigers<br />(under 7 years old)</td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td>17:15-18:00</td>
-                    <td class="kids_a">Youth</td>
-                    <td></td>
-                    <td></td>
-                    <td class="kids_a">Youth</td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td>18:00-19:00</td>
-                    <td class="adult">Adults &amp; Teens</td>
-                    <td class="adult">Adults &amp; Teens</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td>19:00-20:00</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td class="adult">Adults &amp; Teens</td>
-                    <td></td>
-                 </tr>
-            </table>
-        </div>
-
-        <!-- Fall Schedule -->
-        <br/><br/>
-        <div class="row">
-            <div class="span12" style="text-align:center;color:black">
-                <h4>— FALL/SPRING SCHEDULE - September 1st 2013 to June 30th 2014 —</h4>
-            </div>
-        </div>
-
         <div class="row">
             <table class="grid_schedule">
                 <tr>
@@ -154,9 +66,9 @@ require 'includes/header.php';
                 <tr>
                     <td>6:00-7:00</td>
                     <td class="adult">Adults &amp; Teens</td>
-                    <td class="kids_a">Youth</td>
+                    <td class="kids_a">Youth Advanced<br/>(6:05-6:50)</td>
                     <td></td>
-                    <td class="kids_a">Youth</td>
+                    <td class="kids_a">Youth Advanced<br/>(6:05-6:50)</td>
                     <td></td>
                 </tr>
                 <tr>
