48941c6d8b8ff8451d177de9d8c7f9b50a313eed
Jonathan Klein <jonathan.n.klein@gmail.com>
2014-08-14 17:09:14 -0400

    Revert "Updating summer schedule 2014"
    This reverts commit a5d3ff99f980e16b79eafde0c17b37ab5d61ddbb.

--- schedule.php
+++ schedule.php
@@ -1,5 +1,5 @@
 <?php
-$page_name = 'Summer Schedule - July 7th 2014 to August 31st 2014';
+$page_name = 'FALL/SPRING SCHEDULE - September 3rd 2013 to June 30th 2014';
 
 require 'includes/header.php';
 ?>
@@ -15,15 +15,6 @@ require 'includes/header.php';
                     <th>Thursday</th>
                     <th>Friday</th>
                     <th>Saturday</th>
-                </tr>
-                <tr>
-                    <td colspan="7">
-                        <h1>Summer Taekwondo</h1>
-                        9:00am - 12:15pm (Half day)<br/>
-                        9:00am - 3:00pm (Full day)<br/>
-                        One Session is two weeks<br/>
-                        <a href="summer.php">Sign up now!</a>
-                    </td>
                 </tr>
                 <tr>
                     <td>11:00-12:00</td>
@@ -49,30 +40,47 @@ require 'includes/header.php';
                     </td>
                 </tr>
                 <tr>
-                    <td>16:30-17:10</td>
+                    <td>3:50-4:30</td>
+                    <td class="under_seven">Little Tigers<br />(under 7 years old)</td>
+                    <td></td>
                     <td class="under_seven">Little Tigers<br />(under 7 years old)</td>
                     <td></td>
                     <td></td>
+                </tr>
+                <tr>
+                    <td>4:30-5:15</td>
+                    <td class="kids_a">Youth</td>
+                    <td class="under_seven">Little Tigers<br />(under 7 years old)</td>
+                    <td class="kids_a">Youth</td>
                     <td class="under_seven">Little Tigers<br />(under 7 years old)</td>
                     <td></td>
                 </tr>
                 <tr>
-                    <td>17:15-18:00</td>
+                    <td>5:15-6:00</td>
+                    <td class="youth_adv">Youth Advanced</td>
                     <td class="kids_a">Youth</td>
-                    <td></td>
-                    <td></td>
+                    <td class="youth_adv">Youth Advanced</td>
                     <td class="kids_a">Youth</td>
                     <td></td>
                 </tr>
                 <tr>
-                    <td>18:00-19:10</td>
+                    <td>6:00-7:00</td>
                     <td class="adult">Adults &amp; Teens</td>
+                    <td class="youth_adv">Youth Advanced<br/>(6:05-6:50)</td>
                     <td></td>
+                    <td class="youth_adv">Youth Advanced<br/>(6:05-6:50)</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>7:00-8:00</td>
+                    <td></td>
+                    <td class="adult">Adults &amp; Teens</td>
                     <td></td>
                     <td class="adult">Adults &amp; Teens</td>
                     <td></td>
                 </tr>
             </table>
+
         </div>
     </div>
 </section>
