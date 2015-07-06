5ab625ebfca9a6788f33f6cd0670bcd200550a50
Jonathan Klein <jonathan.n.klein@gmail.com>
2015-07-06 19:24:50 -0400

    Updating summer schedule

--- schedule.php
+++ schedule.php
@@ -1,7 +1,7 @@
 <?php
 
-$page_name = 'Spring Schedule - January 2nd 2015 to July 5th 2015';
-$secondary_header = 'Summer Schedule - July 6th 2015 to August 28st 2015';
+$page_name = 'Summer Schedule - July 6th 2015 to August 28st 2015';
+// $secondary_header = 'Summer Schedule - July 6th 2015 to August 28st 2015';
 
 require 'includes/header.php';
 ?>
@@ -9,6 +9,84 @@ require 'includes/header.php';
     <div class="container">
         <div class="row">
             <table class="grid_schedule">
+                <tr>
+                    <th>&nbsp;</th>
+                    <th>Monday</th>
+                    <th>Tuesday</th>
+                    <th>Wednesday</th>
+                    <th>Thursday</th>
+                    <th>Friday</th>
+                    <th>Saturday</th>
+                </tr>
+                <tr>
+                    <td colspan="7">
+                        <h1>Summer Taekwondo</h1>
+                        9:00am - 1pm (Half day)<br/>
+                        9:00am - 3:20pm (Full day)<br/>
+                        One Session is two weeks<br/>
+                        <a href="summer.php">Sign up now!</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>11:00-12:00</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td class="adult">Adults &amp; Teens</td>
+                </tr>
+                <tr class="blank">
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td class="birthday" rowspan="6">
+                        Birthday Party<br/>Available<br/><br/>
+                        <img src="assets/cake.png" height="100px" width="100px" /><br/><br/>
+                        Ask<br/>
+                        For Details<br/>
+                    </td>
+                </tr>
+                <tr>
+                    <td>4:30-5:10</td>
+                    <td class="under_seven">Little Tigers<br />(under 7 years old)</td>
+                    <td></td>
+                    <td></td>
+                    <td class="under_seven">Little Tigers<br />(under 7 years old)</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>5:15-6:00</td>
+                    <td class="kids_a">Youth</td>
+                    <td></td>
+                    <td></td>
+                    <td class="kids_a">Youth</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>6:10-7:10</td>
+                    <td class="adult">Adults &amp; Teens</td>
+                    <td></td>
+                    <td></td>
+                    <td class="adult">Adults &amp; Teens</td>
+                    <td></td>
+                </tr>
+            </table>
+<!--
+            <div class="container section-page-header">
+                <div class="row">
+                    <div class="span12">
+                        <div class="page-header">
+                            <h4>— <?=$secondary_header;?> —</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+<table class="grid_schedule">
                 <tr>
                     <th>&nbsp;</th>
                     <th>Monday</th>
@@ -82,84 +160,8 @@ require 'includes/header.php';
                     <td></td>
                 </tr>
             </table>
-
-            <div class="container section-page-header">
-                <div class="row">
-                    <div class="span12">
-                        <div class="page-header">
-                            <h4>— <?=$secondary_header;?> —</h4>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <table class="grid_schedule">
-                <tr>
-                    <th>&nbsp;</th>
-                    <th>Monday</th>
-                    <th>Tuesday</th>
-                    <th>Wednesday</th>
-                    <th>Thursday</th>
-                    <th>Friday</th>
-                    <th>Saturday</th>
-                </tr>
-                <tr>
-                    <td colspan="7">
-                        <h1>Summer Taekwondo</h1>
-                        9:00am - 1pm (Half day)<br/>
-                        9:00am - 3:20pm (Full day)<br/>
-                        One Session is two weeks<br/>
-                        <a href="summer.php">Sign up now!</a>
-                    </td>
-                </tr>
-                <tr>
-                    <td>11:00-12:00</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td class="adult">Adults &amp; Teens</td>
-                </tr>
-                <tr class="blank">
-                    <td>&nbsp;</td>
-                    <td>&nbsp;</td>
-                    <td>&nbsp;</td>
-                    <td>&nbsp;</td>
-                    <td>&nbsp;</td>
-                    <td>&nbsp;</td>
-                    <td class="birthday" rowspan="6">
-                        Birthday Party<br/>Available<br/><br/>
-                        <img src="assets/cake.png" height="100px" width="100px" /><br/><br/>
-                        Ask<br/>
-                        For Details<br/>
-                    </td>
-                </tr>
-                <tr>
-                    <td>4:30-5:10</td>
-                    <td class="under_seven">Little Tigers<br />(under 7 years old)</td>
-                    <td></td>
-                    <td></td>
-                    <td class="under_seven">Little Tigers<br />(under 7 years old)</td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td>5:15-6:00</td>
-                    <td class="kids_a">Youth</td>
-                    <td></td>
-                    <td></td>
-                    <td class="kids_a">Youth</td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td>6:10-7:10</td>
-                    <td class="adult">Adults &amp; Teens</td>
-                    <td></td>
-                    <td></td>
-                    <td class="adult">Adults &amp; Teens</td>
-                    <td></td>
-                </tr>
-            </table>
+-->
+            
         </div>
     </div>
 </section>
